4cc72d55c2248f210e859d4e2af7e29a833d6944
uniqlogo <45226619+uniqlogo@users.noreply.github.com>
2022-06-28 09:32:25 +0530

    Update settings.py

--- hzeet/settings.py
+++ hzeet/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-li&==p!m_l9m!pe%&=08ff=x^mkfi8lf_7w0!iq(a8l34zb7k#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
